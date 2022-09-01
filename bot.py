412ffcccc2399d36790cb477134bc44d2877fdcb
muzzu2 <63475581+muzzu2@users.noreply.github.com>
2022-09-01 07:42:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,7 +94,7 @@ async def mentionall(event):
       if event.chat_id not in moment_worker:
         await event.respond("Stopped!")
         return
-      if usrnum == 7:
+      if usrnum == 15:
         await decodebot.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
         await asyncio.sleep(2)
         usrnum = 0
@@ -112,7 +112,7 @@ async def mentionall(event):
       if event.chat_id not in moment_worker:
         await event.respond("Stopped")
         return
-      if usrnum == 7:
+      if usrnum == 15:
         await decodebot.send_message(event.chat_id, usrtxt, reply_to=msg)
         await asyncio.sleep(2)
         usrnum = 0
